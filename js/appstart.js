83d9ba68d34a65daecb471ed5d901acdc837a0a8
楠 李 <the.king.homeless.x@gmail.com>
2017-12-13 00:24:29 +0800

    提交基本文件

---
+++ js/appstart.js
@@ -0,0 +1,15 @@
+/*
+ * Create a list that holds all of your cards
+ */
+
+
+/*
+ * сно╥©╙й╪
+ */
+
+$(function () {
+    $(".card").click(function () {
+        debugger;
+        $(this);
+    })
+})
